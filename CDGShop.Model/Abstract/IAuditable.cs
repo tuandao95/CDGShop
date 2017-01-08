0a5eb67ceac00ed7082ef390172d59fafc1c57b1
tuandc <daocongtuanmail@gmail.com>
2017-01-08 17:10:57 +0700

    add domain entities for project

---
+++ CDGShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CDGShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+
+        string CreatedBy { set; get; }
+
+        DateTime? UpdateDate { set; get; }
+
+        string UpdateBy { set; get; }
+
+        string MetaKeyword { set; get; }
+
+        string MetaDescription { set; get; }
+
+        bool Status { set; get; }
+    }
+}
---
+++ CDGShop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CDGShop.Model.Models
+{
+    [Table("VisitorStatistics")]
+    public class VisitorStatistic
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public DateTime VisitedDate { get; set; }
+
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
